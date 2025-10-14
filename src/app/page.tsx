12b6ec63f048bd77533a4bdcb1ddc1743fe265a1
MUHAMMAD ZAKUAN BIN ZULKARNAIN <85609731+Zxck-Nemesiz@users.noreply.github.com>
2025-10-14 11:37:26 +0900

    Base version of Wordle (without API calling for random words generation)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,160 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useState, useEffect, useRef } from "react";
+
+const ROWS = 6;
+const TILES_PER_ROW = 5;
+const TARGET = 'REACT'
+
+function Tile({ letter, color, isAnimating, delay, rowIsRevealed }: { letter?: string, color?: string, isAnimating?: boolean, delay?: number, rowIsRevealed?: boolean| undefined }) {
+  let bgColor = ''
+  const style = { animationDelay: `${delay}ms`}
+  let borderColor = ''
+
+  if (!letter) {
+    borderColor = 'border-gray-300' 
+  } else if (!rowIsRevealed) {
+    borderColor = 'border-gray-500'
+  } else {
+    borderColor = 'border-gray-600' 
+  }
+  
+  if (color === 'green') {
+    bgColor = 'bg-[#6aaa64]'
+  } else if (color === 'yellow') {
+    bgColor = 'bg-[#c9b458]'
+  } else if (color === 'gray') {
+    bgColor = 'bg-[#787c7e]'
+  }
+
+  const classes = `flex justify-center items-center border-2 w-16 h-16 text-2xl uppercase ${borderColor} ${rowIsRevealed ? 'text-white' : 'text-gray-900'} ${rowIsRevealed ? bgColor : ''} ${isAnimating ? 'animate-flip' : ''}`
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className={classes} style={style}>{letter}</div>
+  )
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function Game() {
+  const [guesses, setGuesses] = useState(Array(6).fill(''))
+  const [colors, setColors] = useState(Array(6).fill(null).map(() => Array(5).fill(null))) //color of 6 row, 5 tile
+  const [currRow, setCurrRow] = useState(0)
+  const [gameStatus, setGameStatus] = useState<'playing' | 'won' | 'lost'>('playing')
+  const [animateRow, setAnimateRow] = useState< number| null >(null)
+  const [revealedRows, setRevealedRows] = useState<Set<number>>(new Set())
+
+  const guessesRef = useRef(guesses)
+  const rowRef = useRef(currRow)
+  const colorsRef = useRef(colors)
+  const gameRef = useRef(gameStatus)
+
+  guessesRef.current = guesses
+  rowRef.current = currRow
+  colorsRef.current = colors
+  gameRef.current = gameStatus
+
+  const checkGuess = (guess: string, target: string) => {
+    let i = 0
+    const seen = new Set([...target])
+
+    const newColors = [...colorsRef.current]
+    for(i = 0; i < TILES_PER_ROW; i++) {
+      if (guess[i].toUpperCase() === target[i]) {
+        newColors[rowRef.current][i] = 'green'
+      }
+      else if (seen.has(guess[i].toUpperCase())) {
+        newColors[rowRef.current][i] = 'yellow'
+      }
+      else {
+        newColors[rowRef.current][i] = 'gray'
+      }
+    }
+    setColors(newColors)
+  }
+
+  useEffect(() => {
+    const handleTyping = (e: KeyboardEvent) => {
+      if (gameRef.current !== 'playing') return
+
+      if(guessesRef.current[rowRef.current].length < 5 && /^[a-zA-Z]$/.test(e.key)) {
+        const newGuesses = [...guessesRef.current]
+        newGuesses[rowRef.current] = newGuesses[rowRef.current] + e.key
+        setGuesses(newGuesses)
+      }
+
+      if(e.key === 'Enter') {
+        if(guessesRef.current[rowRef.current].length === 5) {
+          const current = rowRef.current
+          checkGuess(guessesRef.current[rowRef.current], TARGET)
+          setAnimateRow(current)
+
+          setTimeout(() => {
+            setAnimateRow(null)
+            setRevealedRows(prev => new Set(prev).add(current))
+          }, 900)
+          
+          if (guessesRef.current[rowRef.current].toUpperCase() === TARGET) {
+            setGameStatus('won')
+          }
+          else if (rowRef.current === ROWS - 1) {
+            setGameStatus('lost')
+          }
+          else {
+            setCurrRow(rowRef.current + 1)
+          }
+        }
+      }
+
+      if(e.key == 'Backspace') {
+        if(guessesRef.current[rowRef.current].length > 0) {
+          const updatedGuesses = [...guessesRef.current]
+          updatedGuesses[rowRef.current] = guessesRef.current[rowRef.current].slice(0, -1)
+          setGuesses(updatedGuesses)
+        }
+      }
+    }
+    window.addEventListener('keydown', handleTyping)
+
+    return () => {
+      window.removeEventListener('keydown', handleTyping)
+    }
+  }, [])
+
+  const messages = {
+    won: 'You won! 🎉',
+    lost: `Game over! The word was ${TARGET}`
+  }
+
+  const msg_colors = {
+  won: 'text-green-600',
+  lost: 'text-red-600'
+}
+
+  return (
+    <>
+      <div className='bg-gray-100 min-h-[100dvh] flex flex-col justify-center items-center'>
+        <div className="max-w-lg w-full p-6">
+          <div className="mb-12 text-4xl text-center font-bold border-b-2 pb-4">Wordle</div>
+          <div className="flex flex-col items-center gap-4">
+            {Array.from({length: ROWS}, (_, i) => (
+              <div key={i} className="flex gap-1">
+                {Array.from({length: TILES_PER_ROW}, (_, tiles_index) => (
+                  <Tile 
+                  key={tiles_index} 
+                  letter={guesses[i][tiles_index]} 
+                  color={colors[i][tiles_index]} 
+                  isAnimating={i === animateRow} 
+                  delay={tiles_index * 100}
+                  rowIsRevealed={revealedRows.has(i)}/>
+                ))}
+              </div>
+            ))}
+          </div>
+          {gameStatus !== 'playing' && (
+            <div className={`text-center mt-6 text-2xl font-bold ${msg_colors[gameStatus]}`}>{messages[gameStatus]}</div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    </>
+    
+  )
 }
